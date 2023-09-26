a2dc2b71c67c17fe9001d4ad62bf32cb4cf686b0
medakom <elmokademr990@gmail.com>
2023-09-26 11:47:01 +0200

    Initial commit

---
+++ src/main/java/tech/adicharidak/sisanalysis/SisAnalysisApplication.java
@@ -0,0 +1,13 @@
+package tech.adicharidak.sisanalysis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SisAnalysisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SisAnalysisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/adicharidak/sisanalysis/SisAnalysisApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.adicharidak.sisanalysis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SisAnalysisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
